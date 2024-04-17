86ef8e167255cb34ceaee6c53c21d7a7f2d7a9ba
gaikwadsid <sidgaikwad853@gmail.com>
2024-04-17 09:32:45 -0400

    Changed to cookie session

--- App.js
+++ App.js
@@ -7,7 +7,7 @@ import Lab5 from "./Lab5.js";
 import CourseRoutes from "./Kanbas/courses/routes.js";
 import ModuleRoutes from "./Kanbas/modules/routes.js";
 import cors from 'cors';
-import session from "express-session";
+import session from "cookie-session";
 const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/kanbas'
 mongoose.connect(CONNECTION_STRING);
 console.log(CONNECTION_STRING)
